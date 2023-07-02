5e85223650f21ca3272b0c26ae1666c75153738a
Adarsh Ghimire <93777888+Lucifer203@users.noreply.github.com>
2023-07-02 13:56:25 +0545

    Pagination added and skip added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -13,9 +13,15 @@ exports.createProdutct = catchAsyncErrors(async (req, res) => {
 
 //GEt all products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-  const apiFeature = new ApiFeatures(Product.find(), req.query).search();
+  const resultPerPage = 5;
+  const productCount = await Product.countDocuments();
+
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
   const products = await apiFeature.query;
-  res.status(200).json({ success: true, products });
+  res.status(200).json({ success: true, products, productCount });
 });
 
 // Get Single Product with id
