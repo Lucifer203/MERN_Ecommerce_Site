a338dc135a54e825aafccb5a619276c0c5176e5a
Adarsh Ghimire <93777888+Lucifer203@users.noreply.github.com>
2023-07-06 23:22:32 +0545

    added the feature to send email and reset password link

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
 
 //Register User
 exports.registerUser = catchAsyncErrors(async (req, res) => {
@@ -50,4 +51,38 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
     success: true,
     message: "Logged out Successfully",
   });
+});
+
+// Forget Password
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
+  }
+  // Get ResetPassword Token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  const resetPasswordUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/password/reset/${resetToken}`;
+  const message = `Your password reset token is:- \n\n ${resetPasswordUrl} \n\n If you have not requested this email then, please ignore it`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: `Ecommerce Password Recovery`,
+      message,
+    });
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email} succesfully`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(new ErrorHandler(error.message, 500));
+  }
 });
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,10 +4,12 @@ const {
   registerUser,
   LoginUser,
   logout,
+  forgotPassword,
 } = require("../controllers/userController");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(LoginUser);
+router.route("/password/forgot").post(forgotPassword);
 
 router.route("/logout").get(logout);
 module.exports = router;
