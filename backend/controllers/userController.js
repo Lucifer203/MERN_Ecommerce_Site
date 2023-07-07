4aff63ce4624a31594b04a4afb06278a8b32f37a
Adarsh Ghimire <93777888+Lucifer203@users.noreply.github.com>
2023-07-07 22:48:39 +0545

    Update user details added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -144,3 +144,21 @@ exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
   await user.save();
   sendToken(user, 200, res);
 });
+
+// update User Details
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+  //We will add cloudinary later
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -8,6 +8,7 @@ const {
   resetPassword,
   getUserDetails,
   updatePassword,
+  updateProfile,
 } = require("../controllers/userController");
 const { isAuthenticatedUser } = require("../middleware/auth");
 
@@ -17,6 +18,11 @@ router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/logout").get(logout);
+
 router.route("/me").get(isAuthenticatedUser, getUserDetails);
+
 router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+
 module.exports = router;
