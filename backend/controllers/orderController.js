579a699bce8ff3fdbbc5b89fa5ad9126f252610e
Adarsh Ghimire <93777888+Lucifer203@users.noreply.github.com>
2023-07-08 19:47:35 +0545

    Made ordermodel and place new order api

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,32 @@
+const Order = require("../models/orderModel");
+const ErrorHandler = require("../utils/errorhandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const Product = require("../models/productModel");
+
+// Create new Order
+exports.newOrder = catchAsyncErrors(async (req, res, next) => {
+  const {
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+  const order = await Order.create({
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paidAt: Date.now(),
+    user: req.user._id,
+  });
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
