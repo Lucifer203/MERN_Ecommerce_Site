dccfc8972dafd36b73573c0972c402b6960e526a
Adarsh Ghimire <93777888+Lucifer203@users.noreply.github.com>
2023-07-14 17:56:00 +0545

    filter added typography and slider too

--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -7,20 +7,33 @@ import ProductCard from "../Home/ProductCard";
 import { useParams } from "react-router-dom";
 //import { Pagination } from "@mui/material";
 import Pagination from "react-js-pagination";
+import { Slider, Typography } from "@mui/material";
+//import { Typography } from "@mui/material/styles/createTypography";
 
 const Products = () => {
   const dispatch = useDispatch();
+
   const [currentPage, setCurrentPage] = useState(1);
+
+  const [price, setPrice] = useState([0, 25000]);
+
   const { products, loading, error, productsCount, resultPerPage } =
     useSelector((state) => state.products);
+
   const { keyword } = useParams();
 
   const setCurrentPageNo = (e) => {
     setCurrentPage(e);
   };
+
+  const priceHandler = (event, newPrice) => {
+    event.preventDefault();
+    setPrice(newPrice);
+  };
+
   useEffect(() => {
-    dispatch(getProduct(keyword));
-  }, [dispatch, keyword]);
+    dispatch(getProduct(keyword, currentPage));
+  }, [dispatch, keyword, currentPage, price]);
 
   return (
     <Fragment>
@@ -36,22 +49,39 @@ const Products = () => {
               })}
           </div>
 
-          <div className="paginationBox">
-            <Pagination
-              activePage={currentPage}
-              itemsCountPerPage={resultPerPage}
-              totalItemsCount={productsCount}
-              onChange={setCurrentPageNo}
-              nextPageText="Next"
-              prevPageText="Prev"
-              firstPageText="1st"
-              lastPageText="Last"
-              itemClass="page-item"
-              linkClass="page-link"
-              activeClass="pageItemActive"
-              activeLinkClass="pageLinkActive"
+          {/* <select name="" id="">
+            <option value="">200</option>
+          </select> */}
+          <div className="filterBox">
+            <Typography>Price</Typography>
+            <Slider
+              value={price}
+              onChange={priceHandler}
+              valueLabelDisplay="auto"
+              aria-labelledby="range-slider"
+              min={0}
+              max={25000}
             />
           </div>
+
+          {resultPerPage < productsCount && (
+            <div className="paginationBox">
+              <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={resultPerPage}
+                totalItemsCount={productsCount}
+                onChange={setCurrentPageNo}
+                nextPageText="Next"
+                prevPageText="Prev"
+                firstPageText="1st"
+                lastPageText="Last"
+                itemClass="page-item"
+                linkClass="page-link"
+                activeClass="pageItemActive"
+                activeLinkClass="pageLinkActive"
+              />
+            </div>
+          )}
         </Fragment>
       )}
       {/* <Pagination count={10} /> */}
